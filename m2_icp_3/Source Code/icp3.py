a3acbff7c2bca9860fe6238d52d9a130e6c895d6
Zeal Patel <zealpatel1990@gmail.com>
2019-11-05 10:34:17 -0600

    update

--- m2_icp_3/Source Code/icp3.py
+++ m2_icp_3/Source Code/icp3.py
@@ -1,4 +1,4 @@
-import socket, traceback,tweepy,picamera
+import socket, traceback,tweepy#,picamera
 from time import sleep
 
 consumer_key ="QkWOL7X4Ni1v41sE62recwUsm"
@@ -8,45 +8,50 @@ access_token_secret ="8ASK_FOR _IT"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)   
 auth.set_access_token(access_token, access_token_secret) 
 api = tweepy.API(auth) 
-  
 
-def getUdp(port):
-    host = ''
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) 
-    s.bind((host, port)) 
-    try:
-        message, address = s.recvfrom(8192)           
-        return message
-    except (KeyboardInterrupt, SystemExit): 
-        raise 
-    except: 
-        traceback.print_exc()
-def sendUdp(ip,port,msg):
-    UDP_IP = ip
-    UDP_PORT = port
-    MESSAGE = msg
-    print "UDP target IP:", ip
-    print "UDP target port:", port
-    print "message:", msg
-    sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-    sock.sendto(msg, (ip, port))
-camera = picamera.PiCamera()
-camera.resolution = (800, 600)
+def init_server_Tcp(port):
+    TCP_IP = ''
+    TCP_PORT = port
+    BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    print 'Connection address:', addr
+    while 1:
+        data = conn.recv(BUFFER_SIZE)
+        if not data: break
+        #conn.send(data)  # echo
+##    return data
+##    conn.close()
+
+
+#camera = picamera.PiCamera()
+#camera.resolution = (800, 600)
 while (1):
     try:
-        inData = getUdp(5555)
+        TCP_IP = ''
+        TCP_PORT = port
+        BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((TCP_IP, TCP_PORT))
+        s.listen(1)
+        conn, addr = s.accept()
+        print 'Connection address:', addr
+        while 1:
+            data = conn.recv(BUFFER_SIZE)
+            if not data: break
+
+        inData = data
         print ("Data Recevied from Raspberry Pi: ",inData)
         if ("Tweet" in inData) or ("tweet" in inData):
             try:
                 api.update_status(status =inData.replace("Tweet:","")) 
                 print ("Tweet Successful")
-                sendUdp("192.168.43.203",5555,"Tweet Successful")
+                conn.send("Tweet Successful")
             except:
                 print "Error: not posted"
-                sendUdp("192.168.43.203",5555,"Error: not posted")
+                conn.send("Error: not posted")
 
         elif ("Img" in inData) or ("img" in inData):
             camera.capture('snapshot.jpg')
@@ -55,9 +60,9 @@ while (1):
             status = api.update_with_media(image_path, tweet)  
             api.update_status(status = tweet)
             print ("Image tweeted Successfully")
-
+            conn.send("Image tweeted Successfully")
+        conn.close()
     except:
         print ("Some error occured.. program continue")
 
-sendUdp("192.168.43.203",5555,"helloTesgfchbjt msg")
 # twitter link https://twitter.com/Zeal18052398
